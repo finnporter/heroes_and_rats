adc397ba34d7267773f841f60693d5d9896ae5a4
finnporter <iamfinnporter@gmail.com>
2017-07-28 11:46:56 +0100

    adds beforeEach

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -3,7 +3,11 @@ var Hero = require('../hero');
 
 describe("Hero", function() {
 
-  var hero = new Hero("Kerlin Tapatalk", "Shepherds Pie");
+  var hero
+
+  beforeEach(function() {
+    hero = new Hero("Kerlin Tapatalk", "Shepherds Pie")
+  });
 
   it("should have name", function() {
     assert.strictEqual(hero.name, "Kerlin Tapatalk");
